63f3f523038748af84c2b631de00c1ad66db2c68
Ordin Zabek <ordin.zabek@gmail.com>
2023-04-23 16:52:44 +0800

    names are redirecting

--- src/components/Command.jsx
+++ src/components/Command.jsx
@@ -18,58 +18,85 @@ import {
   CommandShortcut,
 } from "@/components/ui/command"
 import { Language } from "@/lib/language";
+import { useRouter } from "next/router";
 
 
 
 
 
 export function CommandDemo() {
-  const groups = ['Love', 'Healing', 'Money', 'Sharing', 'Ego', 'Repentence']
-  const { locale, lang, Pronounced } = Language();
-
-
   return (
     <Command className="rounded-lg border border-cal-500">
       <CommandInput placeholder="Type a command or search..." />
-      <CommandList className="max-h-[500px]" >
+      <CommandList className="max-h-[500px]">
         <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading="Suggestions">
-          {groups.map((group) => (
-            <CommandItem>
-              <Calendar className="mr-2 h-4 w-4" />
-              <span>{group}</span>
-            </CommandItem>
-          ))}
-        </CommandGroup>
+        <Suggestions />
         <CommandSeparator />
-        <CommandGroup heading="All Names">
-          <Names />
-        </CommandGroup>
+        <Names />
       </CommandList>
     </Command>
-  )
+  );
 }
 
+const Suggestions = () => {
+  const groups = ["Love", "Healing", "Money", "Sharing", "Ego", "Repentence"];
+
+  return (
+    <CommandGroup heading="Suggestions">
+      {groups.map((group) => (
+        <CommandItem>
+          <Calendar className="mr-2 h-4 w-4" />
+          <span>{group}</span>
+        </CommandItem>
+      ))}
+    </CommandGroup>
+  );
+};
+
 const Names = () => {
-  const { locale, lang, Pronounced } = Language();
+  const { lang } = Language();
+  const router = useRouter()
+  const names = Object.entries(lang).map(([key, data]) => (
 
 
-  return Object.entries(lang).map(([key, data]) => (
-    <CommandItem>
+    <CommandItem onSelect={() => { router.push(`/${key}`) } }>
       <div className="flex space-x-4">
-        <div className="">#{key}</div>
-
+        <div className="text-cal-500">#{key}</div>
         <div>
-          <div className="flex text-cal-300">
-            {data.purpose}
-          </div>
-          <div className="flex text-cal-500">
-            {data.short}
-          </div>
+          <div className="flex text-cal-200">{data.purpose}</div>
+          <div className="flex text-cal-500">{data.short}</div>
         </div>
-
       </div>
     </CommandItem>
   ));
 
+  return <CommandGroup heading="All Names">{names}</CommandGroup>;
+
 }
+
+const groups = [
+  {
+    title: "Love",
+    items: [12, 17, 28, 30, 35, 44, 53, 58, 68]
+  },
+  {
+    title: "Healing",
+    items: [5, 41, 61]
+  },
+  {
+    title: "Money",
+    items: [41,45, 70]
+  },
+  {
+    title: "Sharing",
+    items: [23, 38, 65]
+  },
+  {
+    title: "Ego",
+    items: [4,11,17,20,30,34,40,43,45,49]
+  },
+  {
+    title: "Repentence",
+    items: [51]
+  }
+]
