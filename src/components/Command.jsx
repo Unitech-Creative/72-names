d69cee509255449fb769b7f75848f1d912802fd8
Ordin Zabek <ordin.zabek@gmail.com>
2023-04-23 22:30:48 +0800

    Command is working

--- src/components/Command.jsx
+++ src/components/Command.jsx
@@ -1,3 +1,4 @@
+import React from "react"
 import {
   Calculator,
   Calendar,
@@ -6,6 +7,7 @@ import {
   Smile,
   User,
 } from "lucide-react"
+
 
 import {
   Command,
@@ -21,58 +23,7 @@ import { Language } from "@/lib/language";
 import { useRouter } from "next/router";
 
 
-
-
-
-export function CommandDemo() {
-  return (
-    <Command className="rounded-lg border border-cal-500">
-      <CommandInput placeholder="Type a command or search..." />
-      <CommandList className="max-h-[500px]">
-        <CommandEmpty>No results found.</CommandEmpty>
-        <Suggestions />
-        <CommandSeparator />
-        <Names />
-      </CommandList>
-    </Command>
-  );
-}
-
-const Suggestions = () => {
-  const groups = ["Love", "Healing", "Money", "Sharing", "Ego", "Repentence"];
-
-  return (
-    <CommandGroup heading="Suggestions">
-      {groups.map((group) => (
-        <CommandItem>
-          <Calendar className="mr-2 h-4 w-4" />
-          <span>{group}</span>
-        </CommandItem>
-      ))}
-    </CommandGroup>
-  );
-};
-
-const Names = () => {
-  const { lang } = Language();
-  const router = useRouter()
-  const names = Object.entries(lang).map(([key, data]) => (
-
-
-    <CommandItem onSelect={() => { router.push(`/${key}`) } }>
-      <div className="flex space-x-4">
-        <div className="text-cal-500">#{key}</div>
-        <div>
-          <div className="flex text-cal-200">{data.purpose}</div>
-          <div className="flex text-cal-500">{data.short}</div>
-        </div>
-      </div>
-    </CommandItem>
-  ));
-
-  return <CommandGroup heading="All Names">{names}</CommandGroup>;
-
-}
+import { useState, useRef } from "react";
 
 const groups = [
   {
@@ -100,3 +51,93 @@ const groups = [
     items: [51]
   }
 ]
+
+export function CommandDemo() {
+
+  const ref = useRef(null)
+  const [open, setOpen] = useState(false)
+  const [search, setSearch] = useState('')
+  const [pages, setPages] = useState([])
+  const page = pages[pages.length - 1]
+
+
+  return (
+    <Command className="rounded-lg border border-cal-500"
+      onKeyDown={(e) => {
+        // Escape goes to previous page
+        // Backspace goes to previous page when search is empty
+        if (e.key === 'Escape' || (e.key === 'Backspace' && !search)) {
+          e.preventDefault()
+          setPages((pages) => pages.slice(0, -1))
+        }
+      }}
+    >
+      <CommandInput
+        placeholder="Type a command or search..."
+        value={search} onValueChange={setSearch}
+      />
+      <CommandList className="max-h-[500px]">
+        <CommandEmpty>No results found.</CommandEmpty>
+        <Suggestions page={page} setPages={setPages} pages={pages} />
+        <CommandSeparator />
+        <Names page={page} />
+      </CommandList>
+    </Command>
+  );
+}
+
+const Suggestions = ({page, setPages, pages}) => {
+
+  return (
+    <CommandGroup heading={page || "Suggestions"}>
+      {groups.map((group) => (
+        !page ? (
+          <CommandItem
+            key={group.id}
+            onSelect={() => setPages([...pages, group.title])}
+          >
+            <Calendar className="mr-2 h-4 w-4" />
+            <span>{group.title}</span>
+          </CommandItem>
+        ) : <React.Fragment key={group.id} />
+      ))}
+    </CommandGroup>
+  );
+
+};
+
+const Names = ({page}) => {
+  const ids = Array.from({ length: 72 }, (_, i) => i + 1);
+  const names = ids.map((id) => {
+
+    const inGroups = groups.filter((group) => group.items.includes(id))
+    const groupTitles = inGroups.map((group) => group.title)
+
+    if( !page || groupTitles.includes(page)) {
+      return <NameCommandItem key={id} id={id} />
+    }else{
+      return <React.Fragment key={id} />
+    }
+  });
+
+  return <CommandGroup heading="All Names">{names}</CommandGroup>;
+}
+
+const NameCommandItem = ({ id }) => {
+  const { lang } = Language();
+  const router = useRouter()
+  const data = lang[id]
+
+  return (
+    <CommandItem key={id} onSelect={() => { router.push(`/${id}`) } }>
+      <div className="flex space-x-4">
+        <div className="text-cal-500">#{id}</div>
+        <div>
+          <div className="flex text-cal-200">{data.purpose}</div>
+          <div className="flex text-cal-500">{data.short}</div>
+        </div>
+      </div>
+    </CommandItem>
+  )
+
+}
