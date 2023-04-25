11238bc09049bd1676fca20b81ec5e7053433431
Ordin Zabek <ordin.zabek@gmail.com>
2023-04-25 18:14:42 +0800

    setup for more languages

--- src/lib/language.jsx
+++ src/lib/language.jsx
@@ -2,29 +2,43 @@ import { useRouter } from "next/router";
 import Pronounced from "@/locales/pronounced.json";
 import EnglishNames from "@/locales/en.json";
 import RussianNames from "@/locales/ru.json";
+// import GermanNames from "@/locales/de.json";
+// import CroatianNames from "@/locales/hr.json";
 import EnglishApp from "@/locales/en.app.json";
 import RussianApp from "@/locales/ru.app.json";
+// import GermanApp from "@/locales/de.app.json";
+// import CroatianApp from "@/locales/hr.app.json";
 import Link from "next/link";
 import { Badge } from "@/components/Badge";
 
 const languages = {
-  ru: "Русский",
   en: "English",
+  ru: "Русский",
   // de: "Deutsch",
   // hr: "Hrvatski",
 };
 
 export const Language = () => {
   let { locale } = useRouter();
-  const names = locale === "ru" ? RussianNames : EnglishNames;
-  const app = locale === "ru" ? RussianApp : EnglishApp;
+
+  const names = {
+    en: EnglishNames,
+    ru: RussianNames,
+    // de: GermanNames,
+    // hr: CroatianNames,
+  }[locale];
+
+  const app = {
+    en: EnglishApp,
+    ru: RussianApp,
+    // de: GermanApp,
+    // hr: CroatianApp,
+  }[locale];
 
   const lang = Object.assign({}, names, app);
 
-
   return { locale, lang, Pronounced };
 };
-
 
 export const LanguageButtons = () => {
   const { locale, asPath } = useRouter();
